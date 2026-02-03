8387ba93af3a69280acca5f9a87109da255ec396
downloadableduck <238381293+downloadableduck@users.noreply.github.com>
2026-02-03 14:03:35 -0800

    Initial commit

---
+++ common/src/test/java/com/jeff/horizon/components/RotationTest.java
@@ -0,0 +1,38 @@
+package com.jeff.horizon.components;
+
+import com.mojang.serialization.JsonOps;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class RotationTest {
+    @ParameterizedTest
+    @ValueSource(strings = {
+            """
+                    {
+                    }
+                    """,
+            """
+                    {
+                        "skyboxRotation": false
+                    }
+                    """,
+            """
+                    {
+                        "keyFrames": {
+                            "0": [0, 0, 0]
+                        }
+                    }
+                    """,
+            """
+                    {
+                        "rotationDuration": 12000
+                    }
+                    """
+    })
+    public void testCorrectParse(String json) {
+        var jsonOb = JsonTestHelper.readJson(json);
+        assertDoesNotThrow(() -> Rotation.CODEC.decode(JsonOps.INSTANCE, jsonOb).getOrThrow().getFirst());
+    }
+}
