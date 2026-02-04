ca7c4015818b8d7e601a60b75bee59e037ea9d19
downloadableduck <238381293+downloadableduck@users.noreply.github.com>
2026-02-03 20:13:34 -0800

    cleaned up the code and removed the dependency on architectury

--- common/src/main/java/com/jeff/horizon/skybox/TextureRegistrar.java
+++
@@ -1,9 +0,0 @@
-package com.jeff.horizon.skybox;
-
-import net.minecraft.resources.ResourceLocation;
-
-import java.util.List;
-
-public interface TextureRegistrar {
-    List<ResourceLocation> getTexturesToRegister();
-}
